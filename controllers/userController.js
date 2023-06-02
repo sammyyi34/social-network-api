bdc55a24f005be488a243d5851959112b8fd05f8
Sammy Yi <sammyhyi@gmail.com>
2023-06-01 20:12:51 -0400

    post route for user and controller work in insomnia and shows in mongodb

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,18 @@ module.exports = {
   async getAllUsers(req, res) {
     try {
       const users = await User.find();
-      res.json('hi');
+      res.json(users);
     } catch (err) {
       res.status(500).json(err);
+    }
+  },
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
     }
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const {
   getAllUsers,
+  createUser,
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getAllUsers);
+router.route('/').get(getAllUsers).post(createUser);
 
 module.exports = router;
