e2fb297b2a2ed972f79319870e6ecafcf2bee1c8
Sammy Yi <sammyhyi@gmail.com>
2023-06-03 16:05:34 -0400

    made get single thought route and controller and works in insomnia

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,6 +9,17 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId }).select('-__v');
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
   async createThought(req, res) {
     try {
       const thought = await Thought.create(req.body);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const {
   getAllThoughts,
   createThought,
+  getSingleThought
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought);
 
 module.exports = router;
